ad31693982e3b8feb1e744c91a078afbffcfe112
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-05-02 19:09:21 -0400

    Add barebones spec

--- build.sbt
+++ build.sbt
@@ -3,5 +3,9 @@ lazy val root = (project in file(".")).
     name := "multihash",
     version := "0.1",
     scalaVersion := "2.11.7",
-    libraryDependencies += "org.typelevel" %% "cats" % "0.4.1"
+    libraryDependencies ++= Seq(
+        "org.typelevel" %% "cats" % "0.4.1",
+        "org.specs2" %% "specs2-core" % "3.7" % "test",
+        "org.specs2" %% "specs2-junit" % "3.7" % "test"
+        )
   )
---
+++ src/test/scala/io/mediachain/multihash/MultiHashSpec.scala
@@ -0,0 +1,24 @@
+package io.mediachain.multihash
+
+import io.mediachain.BaseSpec
+
+object MultiHashSpec extends BaseSpec {
+  def is =
+  s2"""
+    Calculates sha256-based multihash $sha256
+  """
+
+  val str = "3A344E92C8F2EE8F54B8734F90328A946B0A4273DF8DEBA96A33D9C1EADFEFF82E7451663AF284DA025E2A12EFBAE0971D076C2D5AC30BD6A657601BD1FBC7AE"
+  val bytes = str.sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toByte)
+
+  val expectedHashBytes: Array[Byte] =
+    Array(18, 32, 72, -12, 3, 45, -103, -95, 83, -41, -45, -57, 45, 24, 31,
+      48, -9, 61, 107, 78, -117, 108, 109, 109, -119, 35, 2, 98, 113, 66, 27, -50, -100, 116)
+
+  def sha256 = {
+    val multiHash = MultiHash.hashWithSHA256(bytes)
+
+    multiHash.hashType must beEqualTo(MultiHash.sha256)
+    multiHash.bytes must beEqualTo(expectedHashBytes)
+  }
+}
